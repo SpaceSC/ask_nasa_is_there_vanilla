f87c410b013bb9bf99a648b976255a529f3cc7d1
SpaceSC <ceciliasandor.22@gmail.com>
2021-02-08 13:07:25 +0100

    Add comments

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// write async before name of function
 const pageLoad = async () => {
 
+  // await is what you are waiting for
   let data = await fetch('https://api.nasa.gov/planetary/apod?api_key=JZLWuJDR9crbBSjYEFfoziVpdkNQq6FNywPhfzdT')
     .then(response => response.json());
     
@@ -7,6 +9,7 @@ const pageLoad = async () => {
   
   const rootE = document.getElementById("root");
   
+  // insert title-t and image to root
   rootE.insertAdjacentHTML("beforeend", `
     <h1>${data.title}</h1>
     <img src="${data.url}" />
